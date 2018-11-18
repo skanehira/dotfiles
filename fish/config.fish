b91fae3acfb3003088ddd55ae0ed1309b6f544f5
skanehira <sho19921005@gmail.com>
2018-11-18 10:48:26 +0900

    Add fzf options

--- fish/config.fish
+++ fish/config.fish
@@ -5,6 +5,7 @@ set -x PATH $PATH $GOBIN $HOME/.cargo/bin /usr/local/bin
 set -x XDG_CONFIG_HOME $HOME/.config
 set -x LANG "ja_JP.UTF-8"
 set -x HOMEBREW_UPGRADE_CLEANUP 1
+set -x FZF_DEFAULT_OPTS "--layout=reverse --inline-info"
 
 # alias
 alias lg="lazygit"
