cee2faf4b3ac728b358b212a5e9d1af0a1cdfb72
skanehira <sho19921005@gmail.com>
2019-12-29 04:13:01 +0900

    update fish config

--- linux/fish/config.fish
+++ linux/fish/config.fish
@@ -1,6 +1,6 @@
 set -x GOPATH $HOME/dev/go
 set -x GOBIN $GOPATH/bin
-set -x PATH $PATH $GOBIN /home/skanehira/.local/bin
+set -x PATH $PATH $GOBIN
 set -x FZF_DEFAULT_OPTS "--layout=reverse --inline-info --select-1 --exit-0 -m"
 set -x EDITOR vim
 set -x LC_CTYPE "en_US.UTF-8"
@@ -8,5 +8,5 @@ set -x GHQ_ROOT $GOPATH/src
 set -x XDG_CONFIG_HOME $HOME/.config
 
 alias lg="lazygit"
-alias buildvim='cd $GOPATH/src/github.com/vim/vim/src && git pull && sudo make distclean && ./configure --enable-gui=gtk3 --enable-fail-if-missing && make && sudo make install'
+alias buildvim='cd $GOPATH/src/github.com/vim/vim/src && git pull && sudo make distclean && ./configure --with-x --enable-multibyte --enable-fail-if-missing && make && sudo make install'
 
