78a91eaec14f44204f44cd8764217c2698235c8b
skanehira <sho19921005@gmail.com>
2020-01-06 08:14:57 +0900

    remove unused fish function

--- mac/fish/functions/__bobthefish_display_colors.fish
+++
@@ -1,3 +0,0 @@
-function __bobthefish_display_colors -d 'Print example prompts using the current color scheme'
-  bobthefish_display_colors
-end
--- mac/fish/functions/fvim.fish
+++
@@ -1,7 +0,0 @@
-function fvim -d 'fzf vim open file'
-    if count $argv > /dev/null
-        vim $argv[1]
-    else
-        vim (fzf)
-    end
-end
