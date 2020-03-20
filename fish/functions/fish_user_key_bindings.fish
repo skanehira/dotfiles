9050a82bca9db22f13d6c6c476a0a7900e08f5de
skanehira <sho19921005@gmail.com>
2020-03-20 22:34:53 +0900

    update fish_user_key_bindings.fish

--- fish/functions/fish_user_key_bindings.fish
+++ fish/functions/fish_user_key_bindings.fish
@@ -8,6 +8,9 @@ function fish_user_key_bindings
     bind -M $mode \cp history-search-backward
     bind -M $mode \cn history-search-forward
 
+    bind -M $mode \ca beginning-of-line
+    bind -M $mode \ce end-of-line
+
     # restore fzf keybinding
     # https://github.com/junegunn/fzf/blob/3918c45ceda5e7c57d6832cdeedbfeb8f7a6444e/shell/key-bindings.fish#L94-L96
     bind -M $mode \ct fzf-file-widget
