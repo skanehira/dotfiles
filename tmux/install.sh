7afff4381c8d8db8d8a1757034055aac3d2e0160
skanehira <sho19921005@gmail.com>
2020-04-12 00:04:28 +0900

    merge tmux

--- tmux/install.sh
+++ tmux/install.sh
@@ -5,4 +5,11 @@ fi
 
 if [[ ! -e $HOME/.tmux.conf ]]; then
     ln -s $PWD/tmux.conf ~/.tmux.conf
+
+    case "$(uname)" in
+      "Linux" )
+        ln -s $PWD/tmux.conf.linux ~/.tmux.conf.linux;;
+      "Darwin" )
+        ln -s $PWD/tmux.conf.mac ~/.tmux.conf.mac;;
+    esac
 fi
