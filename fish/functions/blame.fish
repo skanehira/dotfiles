063b452f693aff5f4eb7d803e150a48c79daa32a
skanehira <sho19921005@gmail.com>
2020-09-24 22:56:03 +0900

    update blame

--- fish/functions/blame.fish
+++ fish/functions/blame.fish
@@ -10,11 +10,29 @@ function blame
         return
     end
 
-    git blame --show-number --color-by-age --color-lines $file | fzf --ansi --tiebreak=index \
-        --bind "ctrl-m:execute:
-                (grep -o '[a-f0-9]\{7\}' | head -1 |
-                xargs -I % sh -c 'git show --color=always % | less -R') << 'FZF-EOF'
-                {} FZF-EOF"
+    set id (git blame --show-number --color-by-age --color-lines $file | fzf --ansi --tiebreak=index | cut -f1 -d ' ')
 
+    set log (git log --oneline $id | head -n 1)
+
+    set merge (echo $log | grep 'Merge pull request #')
+    if test -z "$merge"
+      set squash (echo $log | grep '#\d')
+      if test -z "$squash"
+        return
+      end
+      set pr (git log --oneline $id | sed -n 1P | sed 's/\(.*(#\)\(.*\))/pull\/\2/')
+    else
+      set pr (echo $merge | cut -f5 -d ' ' | sed -e 's%#%pull/%')
+    end
+    if test -z "$pr"
+      return
+    end
+
+    set remote (git remote get-url --push origin | sed 's/\(ssh:\/\/git@\)\(.*\)\.git/https:\/\/\2/')
+    if test -z "$remote"
+      return
+    end
+
+    echo $remote/$pr
 end
 
