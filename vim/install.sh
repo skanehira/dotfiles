73a062e0b56a4662d72e564f59e2409d700c9cad
skanehira <sho19921005@gmail.com>
2020-10-21 16:30:47 +0900

    update vim and tmux config

--- vim/install.sh
+++ vim/install.sh
@@ -5,10 +5,6 @@ if [ ! -e $path ]; then
     mkdir -p $path
 fi
 
-if [[ ! -e $path/dein.toml ]]; then
-    ln -s $PWD/dein.toml $path/dein.toml
-fi
-
 if [[ ! -e $HOME/.vimrc ]]; then
     ln -s $PWD/vimrc $HOME/.vimrc
 fi
