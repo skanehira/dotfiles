155e38118741f6dcb834ec66f6f65713a3f8beda
skanehira <sho19921005@gmail.com>
2020-10-27 13:14:19 +0900

    imporve tmuxpopup

--- fish/functions/tmuxpopup.fish
+++ fish/functions/tmuxpopup.fish
@@ -1,10 +1,16 @@
 function tmuxpopup -d "toggle tmux popup window"
-  set width '80%'
-  set height '80%'
-  set session (tmux display-message -p -F "#{session_name}")
-  if contains "popup" $session
-    tmux detach-client
+  set width '90%'
+  set height '90%'
+
+  if count $argv > /dev/null
+    tmux popup -d '#{pane_current_path}' -xC -yC -w$width -h$height -K -E -R $argv[1]
   else
-    tmux popup -d '#{pane_current_path}' -xC -yC -w$width -h$height -K -E -R "tmux attach -t popup || tmux new -s popup"
+    set session (tmux display-message -p -F "#{session_name}")
+    if contains "popup" $session
+      tmux detach-client
+    else
+      tmux popup -d '#{pane_current_path}' -xC -yC -w$width -h$height -K -E -R "tmux attach -t popup || tmux new -s popup"
+    end
   end
+
 end
