a1904c14942f81439e7c808a8a4028f95c491cd1
skanehira <sho19921005@gmail.com>
2021-03-31 07:42:00 +0900

    fix cannot work tmux popup

--- fish/functions/tmuxpopup.fish
+++ fish/functions/tmuxpopup.fish
@@ -3,13 +3,13 @@ function tmuxpopup -d "toggle tmux popup window"
   set height '90%'
 
   if count $argv > /dev/null
-    tmux popup -d '#{pane_current_path}' -xC -yC -w$width -h$height -K -E -R $argv[1]
+    tmux popup -d '#{pane_current_path}' -xC -yC -w$width -h$height -E $argv[1]
   else
     set session (tmux display-message -p -F "#{session_name}")
     if contains "popup" $session
       tmux detach-client
     else
-      tmux popup -d '#{pane_current_path}' -xC -yC -w$width -h$height -K -E -R "tmux attach -t popup || tmux new -s popup"
+      tmux popup -d '#{pane_current_path}' -xC -yC -w$width -h$height -E "tmux attach -t popup || tmux new -s popup"
     end
   end
 
