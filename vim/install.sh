398ee4b5792cfba1e78134afc981f85ca4fd42ae
skanehira <sho19921005@gmail.com>
2021-07-07 00:09:52 +0900

    merge plugin.vim to vimrc

--- vim/install.sh
+++ vim/install.sh
@@ -9,10 +9,6 @@ if [[ ! -e $HOME/.vimrc ]]; then
     ln -s $PWD/vimrc $HOME/.vimrc
 fi
 
-if [[ ! -e $HOME/.plugin.vim ]]; then
-    ln -s $PWD/plugin.vim $HOME/.plugin.vim
-fi
-
 if [[ ! -e $path/sonictemplate ]]; then
     ln -s $PWD/sonictemplate $path/sonictemplate
 fi
