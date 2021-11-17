b3bdb4fdc48945c342be8f4d1ec02f9f7e7a8d24
skanehira <sho19921005@gmail.com>
2021-11-18 04:08:37 +0900

    update dotfiles

--- vim/install.sh
+++ vim/install.sh
@@ -1,14 +1,23 @@
 #!/bin/bash
 path=~/.vim
+nvim_path=$XDG_CONFIG_HOME/nvim
 
 if [ ! -e $path ]; then
-    mkdir -p $path
+  mkdir -p $path
+fi
+
+if [ ! -e $XDG_CONFIG_HOME/nvim ]; then
+  mkdir -p $nvim_path
 fi
 
 if [[ ! -e $HOME/.vimrc ]]; then
-    ln -s $PWD/vimrc $HOME/.vimrc
+  ln -s $PWD/vimrc $HOME/.vimrc
 fi
 
 if [[ ! -e $path/sonictemplate ]]; then
-    ln -s $PWD/sonictemplate $path/sonictemplate
+  ln -s $PWD/sonictemplate $path/sonictemplate
+fi
+
+if [[ ! -e $nvim_path/init.vim ]]; then
+  ln -s $HOME/.vimrc $nvim_path/init.vim
 fi
--- zsh/install.sh
+++ zsh/install.sh
@@ -7,3 +7,7 @@ fi
 if [[ ! -e $HOME/.zshrc ]]; then
     ln -s $PWD/zshrc $HOME/.zshrc
 fi
+
+if [[ ! -e $HOME/.zprofile ]]; then
+    ln -s $PWD/zprofile $HOME/.zprofile
+fi
