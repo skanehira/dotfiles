3689736b22357973bbfbaf3bb74fb29d0be77d14
skanehira <sho19921005@gmail.com>
2022-09-13 11:08:29 +0900

    show file full path in statusline

--- vim/init.lua
+++ vim/init.lua
@@ -414,7 +414,16 @@ require('packer').startup(function(use)
     'nvim-lualine/lualine.nvim',
     requires = 'kyazdani42/nvim-web-devicons',
     config = function()
-      require('lualine').setup({})
+      require('lualine').setup({
+        sections = {
+          lualine_c = {
+            {
+              'filename',
+              path = 3,
+            }
+          }
+        }
+      })
     end
   }
 
