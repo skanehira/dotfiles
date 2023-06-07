3de44300bfe000b436ea24f1ea58492f46dcafeb
skanehira <sho19921005@gmail.com>
2023-06-07 23:03:33 +0900

    vim: add prettier config

--- vim/init.lua
+++ vim/init.lua
@@ -1031,7 +1031,18 @@ require("lazy").setup({
       local null_ls = require('null-ls')
       null_ls.setup({
         sources = {
-          null_ls.builtins.formatting.prettier,
+          null_ls.builtins.formatting.prettier.with {
+            prefer_local = 'node_modules/.bin',
+            condition = function(utils)
+              return utils.root_has_file {
+                '.textlintrc',
+                '.textlintrc.js',
+                '.textlintrc.json',
+                '.textlintrc.yml',
+                '.textlintrc.yaml',
+              }
+            end,
+          },
           null_ls.builtins.diagnostics.actionlint,
           null_ls.builtins.diagnostics.textlint.with {
             prefer_local = 'node_modules/.bin',
