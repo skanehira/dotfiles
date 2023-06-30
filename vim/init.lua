ccd6f3165c6ae918a0b8647cb29703055d28f763
skanehira <sho19921005@gmail.com>
2023-07-01 00:38:43 +0900

    vim: add dial.nvim

--- vim/init.lua
+++ vim/init.lua
@@ -1319,6 +1319,20 @@ require("lazy").setup({
   { 'vim-jp/vital.vim' },
   { 'thinca/vim-themis',         ft = 'vim' },
   { 'tyru/capture.vim' },
+  {
+    'monaqa/dial.nvim',
+    config = function()
+      local dial = require("dial.map")
+      nmap("<C-a>", dial.inc_normal())
+      nmap("<C-x>", dial.dec_normal())
+      nmap("g<C-a>", dial.inc_gnormal())
+      nmap("g<C-x>", dial.dec_gnormal())
+      vmap("<C-a>", dial.inc_visual())
+      vmap("<C-x>", dial.dec_visual())
+      vmap("g<C-a>", dial.inc_gvisual())
+      vmap("g<C-x>", dial.dec_gvisual())
+    end
+  },
 
   -- other
   -- {
