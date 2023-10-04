7d96fdc34b84cbfe48afa89854fc3806ac0bc149
skanehira <sho19921005@gmail.com>
2023-10-04 17:39:12 +0900

    vim: add octo.nvim

--- vim/init.lua
+++ vim/init.lua
@@ -1369,5 +1369,16 @@ require("lazy").setup({
         },
       })
     end
+  },
+  {
+    'pwntester/octo.nvim',
+    dependencies = {
+      'nvim-lua/plenary.nvim',
+      'nvim-telescope/telescope.nvim',
+      'kyazdani42/nvim-web-devicons',
+    },
+    config = function()
+      require('octo').setup()
+    end
   }
 })
