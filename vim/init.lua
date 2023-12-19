374b398638f9161c3513916cc1b3344577b83152
skanehira <sho19921005@gmail.com>
2023-12-20 00:20:55 +0900

    vim: rename VertSplit to WinSeparator

--- vim/init.lua
+++ vim/init.lua
@@ -442,7 +442,7 @@ local colorscheme_config = function()
   opt.termguicolors = true
   cmd([[
       colorscheme carbonfox
-      hi VertSplit guifg=#535353
+      hi WinSeparator guifg=#535353
       hi Visual ctermfg=159 ctermbg=23 guifg=#b3c3cc guibg=#384851
       hi DiffAdd guifg=#25be6a
       hi DiffDelete guifg=#ee5396
