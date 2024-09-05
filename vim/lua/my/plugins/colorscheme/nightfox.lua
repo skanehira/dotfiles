cd91d409363980ab2b61d694ddaddb40cb637909
skanehira <sho19921005@gmail.com>
2024-09-05 22:08:30 +0900

    Revert "vim: remove unnecessary lines from nightfox.lua"
    This reverts commit ba53f539474686b46da1053b363e2195eb0cc64b.

--- vim/lua/my/plugins/colorscheme/nightfox.lua
+++ vim/lua/my/plugins/colorscheme/nightfox.lua
@@ -2,10 +2,11 @@ local config = function()
   vim.opt.termguicolors = true
   vim.cmd([[
       colorscheme carbonfox
+      hi WinSeparator guifg=#535353
+      hi Visual ctermfg=159 ctermbg=23 guifg=#b3c3cc guibg=#384851
       hi DiffAdd guifg=#25be6a
       hi DiffDelete guifg=#ee5396
       ]])
-
 end
 
 local nightfox = {
