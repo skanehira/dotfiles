334e6ca73dfec88263043f9e86de5b6454b4760e
skanehira <sho19921005@gmail.com>
2025-01-10 01:07:26 +0900

    vim: add CopilotChat - Prompt actions

--- vim/lua/my/plugins/completion/copilot-chat.lua
+++ vim/lua/my/plugins/completion/copilot-chat.lua
@@ -8,6 +8,16 @@ local copilotChat = {
   -- opts がないとコマンド出てこない
   opts = {
   },
+  keys = {
+    {
+      "<Leader>cc",
+      function()
+        local actions = require("CopilotChat.actions")
+        require("CopilotChat.integrations.telescope").pick(actions.prompt_actions())
+      end,
+      desc = "CopilotChat - Prompt actions",
+    },
+  }
 }
 
 return copilotChat
