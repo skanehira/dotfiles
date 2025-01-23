bc5430500f4611602903ded0047b444f253a54b8
skanehira <sho19921005@gmail.com>
2025-01-23 23:42:11 +0900

    vim: use deno in ZennCreateArticle

--- vim/lua/my/command.lua
+++ vim/lua/my/command.lua
@@ -3,6 +3,6 @@ vim.api.nvim_create_user_command('ZennCreateArticle',
   function(opts)
     local date = vim.fn.strftime('%Y-%m-%d')
     local slug = date .. '-' .. opts.args
-    os.execute('npx zenn new:article --emoji 🦍 --slug ' .. slug)
+    os.execute('deno run -A  npm:zenn-cli@latest new:article --emoji 🦍 --slug ' .. slug)
     vim.cmd('edit ' .. string.format('articles/%s.md', slug))
   end, { nargs = 1 })
