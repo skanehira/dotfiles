b1b5f6fabda3f538e3b88d5b4130a16e797410d7
skanehira <sho19921005@gmail.com>
2025-02-13 19:03:05 +0900

    vim: remove render-markdown.nvim from avante.nvim dependencies

--- vim/lua/my/plugins/ai/avante.lua
+++ vim/lua/my/plugins/ai/avante.lua
@@ -37,14 +37,6 @@ local avante = {
         },
       },
     },
-    {
-      -- Make sure to set this up properly if you have lazy=true
-      'MeanderingProgrammer/render-markdown.nvim',
-      opts = {
-        file_types = { 'markdown', 'Avante' },
-      },
-      ft = { 'markdown', 'Avante' },
-    },
   },
 }
 
