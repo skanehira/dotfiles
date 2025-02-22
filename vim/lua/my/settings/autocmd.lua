dc234b78e2a05693e0e0bae861654384a9d873ab
skanehira <sho19921005@gmail.com>
2025-02-22 23:15:16 +0900

    vim: adjustment deno buffer

--- vim/lua/my/settings/autocmd.lua
+++ vim/lua/my/settings/autocmd.lua
@@ -122,3 +122,42 @@ vim.api.nvim_create_autocmd("FileType", {
   command = "nnoremap <buffer> <silent>q :bw!<CR>",
   group = vim.api.nvim_create_augroup("helpKeymaps", { clear = true }),
 })
+
+-- for deno lsp
+-- ref :https://github.com/neovim/neovim/issues/30908#issuecomment-2657220629
+local function virtual_text_document(params)
+  local bufnr = params.buf
+  local actual_path = params.match:sub(1)
+
+  local clients = vim.lsp.get_clients({ name = "denols" })
+  if #clients == 0 then
+    return
+  end
+
+  local client = clients[1]
+  local method = "deno/virtualTextDocument"
+  local req_params = { textDocument = { uri = actual_path } }
+  local response = client.request_sync(method, req_params, 2000, 0)
+  if not response or type(response.result) ~= "string" then
+    return
+  end
+
+  local lines = vim.split(response.result, "\n")
+  vim.api.nvim_buf_set_lines(bufnr, 0, -1, false, lines)
+  vim.api.nvim_set_option_value("readonly", true, { buf = bufnr })
+  vim.api.nvim_set_option_value("modified", false, { buf = bufnr })
+  vim.api.nvim_set_option_value("modifiable", false, { buf = bufnr })
+  vim.api.nvim_buf_set_name(bufnr, actual_path)
+  vim.lsp.buf_attach_client(bufnr, client.id)
+
+  local filetype = "typescript"
+  if actual_path:sub(-3) == ".md" then
+    filetype = "markdown"
+  end
+  vim.api.nvim_set_option_value("filetype", filetype, { buf = bufnr })
+end
+
+vim.api.nvim_create_autocmd({ "BufReadCmd" }, {
+  pattern = { "deno:/*" },
+  callback = virtual_text_document,
+})
