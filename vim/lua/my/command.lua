ec5ee832e00e4d1c20a12003dfdda6b3749aaad8
skanehira <sho19921005@gmail.com>
2025-05-08 22:19:42 +0900

    vim: add TerminalExec and gk to switch branch

--- vim/lua/my/command.lua
+++ vim/lua/my/command.lua
@@ -6,3 +6,27 @@ vim.api.nvim_create_user_command('ZennCreateArticle',
     os.execute('deno run -A  npm:zenn-cli@latest new:article --emoji 🦍 --slug ' .. slug)
     vim.cmd('edit ' .. string.format('articles/%s.md', slug))
   end, { nargs = 1 })
+
+vim.api.nvim_create_user_command("TerminalExec", function(opts)
+  local cmd = opts.args
+  vim.cmd("botright 20new")
+  local buf = vim.api.nvim_get_current_buf()
+  vim.fn.jobstart(
+    { "zsh", "-c", "source ~/.zshrc && " .. cmd },
+    {
+      stdout_buffered = true,
+      stderr_buffered = true,
+      pty = true,
+      term = true,
+      on_exit = function(_, code, _)
+        if code == 0 then
+          vim.schedule(function()
+            if vim.api.nvim_buf_is_valid(buf) then
+              vim.api.nvim_buf_delete(buf, { force = true })
+            end
+          end)
+        end
+      end,
+    }
+  )
+end, { nargs = "+", complete = "shellcmd" })
