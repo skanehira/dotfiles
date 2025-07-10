7aecc065f6af4ec7006a3520a8d76bfdd1001a4e
skanehira <sho19921005@gmail.com>
2025-07-10 22:01:08 +0900

    claude: add hooks for notification

---
+++ claude/hooks/notify.ts
@@ -0,0 +1,33 @@
+import { parseArgs } from "jsr:@std/cli/parse-args";
+import $ from "jsr:@david/dax";
+
+type Notification = {
+  session_id: string;
+  transcript_path: string;
+  message: string;
+  title: string;
+};
+
+const flags = parseArgs(Deno.args, {
+  string: ["type"],
+});
+
+// When called from Notification hooks
+async function notify() {
+  const input: Notification = await new Response(Deno.stdin.readable).json();
+  await $`terminal-notifier -title "${input.title}" -message "${input.message}" -sound default`;
+}
+
+// When called from Stop hooks
+async function notifyWhenStop() {
+  await $`terminal-notifier -title "Claude Code" -message "Wait next action" -sound default`;
+}
+
+switch (flags.type) {
+  case "notify":
+    await notify();
+    break;
+  case "stop":
+    await notifyWhenStop();
+    break;
+}
