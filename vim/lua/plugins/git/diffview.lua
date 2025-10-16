cd56010efc9297b35e42c4728c58ff5499adac49
skanehira <sho19921005@gmail.com>
2025-10-16 15:19:26 +0900

    vim: update diffview key mappings

--- vim/lua/plugins/git/diffview.lua
+++ vim/lua/plugins/git/diffview.lua
@@ -2,10 +2,11 @@ local utils = require('utils')
 local nmap = utils.keymaps.nmap
 
 local config = function()
-  nmap('ms', '<Cmd>DiffviewOpen<CR>')
+  nmap('<C-g>d', '<Cmd>DiffviewOpen<CR>')
+  nmap('<C-g>f', '<Cmd>DiffviewFileHistory<CR>')
 
   vim.api.nvim_create_autocmd('FileType', {
-    pattern = 'DiffviewFiles',
+    pattern = 'Diffview*',
     callback = function()
       nmap('q', '<Cmd>tabclose<CR>', { buffer = true })
     end,
