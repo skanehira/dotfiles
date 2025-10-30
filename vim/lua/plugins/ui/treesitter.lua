466f20be8cee3347f30550bacc8ef97ca384b365
skanehira <sho19921005@gmail.com>
2025-10-30 09:03:49 +0900

    vim: add nvim-treesitter-textobjects

--- vim/lua/plugins/ui/treesitter.lua
+++ vim/lua/plugins/ui/treesitter.lua
@@ -22,13 +22,41 @@ local config = function()
     highlight = {
       enable = true,
       disable = { 'yaml' },
-    }
+    },
+    textobjects = {
+      select = {
+        enable = true,
+
+        -- Automatically jump forward to textobj, similar to targets.vim
+        lookahead = true,
+
+        keymaps = {
+          ["af"] = "@function.outer",
+          ["if"] = "@function.inner",
+        },
+        -- You can choose the select mode (default is charwise 'v')
+        --
+        -- Can also be a function which gets passed a table with the keys
+        -- * query_string: eg '@function.inner'
+        -- * method: eg 'v' or 'o'
+        -- and should return the mode ('v', 'V', or '<c-v>') or a table
+        -- mapping query_strings to modes.
+        selection_modes = {
+          ['@parameter.outer'] = 'v', -- charwise
+          ['@function.outer'] = 'V',  -- linewise
+        },
+        include_surrounding_whitespace = true,
+      },
+    },
   })
 end
 
 local treesitter = {
   'nvim-treesitter/nvim-treesitter',
   build = ':TSUpdate',
+  dependencies = {
+    'nvim-treesitter/nvim-treesitter-textobjects',
+  },
   config = config
 }
 
