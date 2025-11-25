0833c6e07d5a1c3ccbc99f6bc174c9dc1e855426
skanehira <sho19921005@gmail.com>
2025-11-25 18:49:45 +0900

    vim: add keymap for github-actions.nvim

--- vim/lua/plugins/etc/github-actions.lua
+++ vim/lua/plugins/etc/github-actions.lua
@@ -2,11 +2,13 @@ return {
   "skanehira/github-actions.nvim",
   dependencies = {
     'nvim-treesitter/nvim-treesitter',
+    'nvim-telescope/telescope.nvim',
   },
   config = function()
     local actions = require('github-actions')
     vim.keymap.set('n', '<leader>gd', actions.dispatch_workflow, { desc = 'Dispatch workflow' })
-    vim.keymap.set('n', '<leader>gh', actions.show_history, { desc = 'Dispatch workflow' })
+    vim.keymap.set('n', '<leader>gh', actions.show_history, { desc = 'Show hisotry of workflows' })
+    vim.keymap.set('n', '<leader>gw', actions.watch_workflow, { desc = 'Watch workflows' })
     actions.setup({});
   end,
 }
