07e85af20b4758d10e231d105d7f629fa6ef875d
skanehira <kanehira.sho@techlead-it.com>
2025-12-26 19:36:01 +0900

    vim: change keymaps for github-actions

--- vim/lua/plugins/github/github-actions.lua
+++ vim/lua/plugins/github/github-actions.lua
@@ -6,9 +6,12 @@ return {
   },
   config = function()
     local actions = require('github-actions')
-    vim.keymap.set('n', '<leader>gd', actions.dispatch_workflow, { desc = 'Dispatch workflow' })
-    vim.keymap.set('n', '<leader>gh', actions.show_history, { desc = 'Show hisotry of workflows' })
-    vim.keymap.set('n', '<leader>gw', actions.watch_workflow, { desc = 'Watch workflows' })
+    vim.keymap.set('n', '<leader>gad', actions.dispatch_workflow, { desc = 'Dispatch workflow' })
+    vim.keymap.set('n', '<leader>gah', actions.show_history, { desc = 'Show hisotry of workflows' })
+    vim.keymap.set('n', '<leader>gaw', actions.watch_workflow, { desc = 'Watch workflows' })
+    vim.keymap.set('n', '<leader>gap', function()
+      actions.show_history({ pr_mode = true })
+    end, { desc = 'Watch workflows' })
     actions.setup({
       actions = {
         enabled = false,
