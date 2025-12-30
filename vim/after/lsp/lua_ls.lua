a9513b5f7edb57f30217c9956058e263dacd4bc1
skanehira <kanehira.sho@techlead-it.com>
2025-12-30 11:32:04 +0900

    vim: update lua_ls.lua

--- vim/after/lsp/lua_ls.lua
+++ vim/after/lsp/lua_ls.lua
@@ -5,18 +5,10 @@ return {
       runtime = {
         version = 'LuaJIT',
       },
-      diagnostics = {
-        -- Get the language server to recognize the `vim` global
-        globals = { 'vim' },
-      },
       workspace = {
-        library = vim.list_extend(vim.api.nvim_get_runtime_file('lua', true), {
-          vim.fs.joinpath(vim.fn.stdpath('config') --[[@as string]], 'lua'),
-          vim.fs.joinpath(vim.env.VIMRUNTIME, "lua"),
-          '${3rd}/luv/library',
-          '${3rd}/busted/library',
-          '${3rd}/luassert/library',
-        }),
+        library = {
+          vim.env.VIMRUNTIME .. '/lua/vim',
+        },
       },
     },
   },
