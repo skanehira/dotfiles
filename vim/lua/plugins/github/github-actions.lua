7cdfffc6495279e4bc31d6b325faab93789941fb
skanehira <kanehira.sho@techlead-it.com>
2026-02-02 10:40:19 +0900

    vim: add keymap for github-actions

--- vim/lua/plugins/github/github-actions.lua
+++ vim/lua/plugins/github/github-actions.lua
@@ -9,9 +9,12 @@ return {
     vim.keymap.set('n', '<leader>gad', actions.dispatch_workflow, { desc = 'Dispatch workflow' })
     vim.keymap.set('n', '<leader>gah', actions.show_history, { desc = 'Show hisotry of workflows' })
     vim.keymap.set('n', '<leader>gaw', actions.watch_workflow, { desc = 'Watch workflows' })
+    vim.keymap.set('n', '<leader>gao', function()
+      require('github-actions').open_workflow_url()
+    end, { buffer = true, desc = 'Open workflow URL in browser' })
     vim.keymap.set('n', '<leader>gap', function()
       actions.show_history({ pr_mode = true })
-    end, { desc = 'Watch workflows' })
+    end, { desc = 'Watch PR workflows' })
     actions.setup({
       actions = {
         enabled = false,
